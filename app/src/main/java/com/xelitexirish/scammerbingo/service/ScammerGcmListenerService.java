552e7130d5d92e2ea89c7777692b7a16b48f7a9f
XeliteXirish <shaunoneill7@me.com>
2016-07-24 13:46:07 +0100

    Layout fix and work on Gcm notifications

---
+++ app/src/main/java/com/xelitexirish/scammerbingo/service/ScammerGcmListenerService.java
@@ -0,0 +1,52 @@
+package com.xelitexirish.scammerbingo.service;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.android.gms.gcm.GcmListenerService;
+import com.xelitexirish.scammerbingo.MainActivity;
+
+public class ScammerGcmListenerService extends GcmListenerService{
+
+    /**
+     * Called when message is received.
+     *
+     * @param from SenderID of the sender.
+     * @param data Data bundle containing message data as key/value pairs.
+     *             For Set of keys use data.keySet().
+     */
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        String message = data.getString("message");
+
+        if(from.startsWith("/topic/")){
+
+        }else{
+
+        }
+
+        sendNotification(message);
+    }
+
+    private void sendNotification(String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setContentTitle("GCM Message");
+        notificationBuilder.setContentText(message);
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
