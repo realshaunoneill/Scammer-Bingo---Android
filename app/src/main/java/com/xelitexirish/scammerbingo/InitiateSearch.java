7e41658e534c8786898aabdb824af50d33c3d9c4
Andrew Fluck <Andrew Fluck>
2016-08-02 12:18:24 -0400

    Material Search

--- app/src/main/java/com/xelitexirish/scammerbingo/InitiateSearch.java
+++ app/src/main/java/com/xelitexirish/scammerbingo/InitiateSearch.java
@@ -4,8 +4,12 @@ import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Build;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.graphics.drawable.DrawerArrowDrawable;
@@ -23,27 +27,9 @@ import android.widget.ImageView;
 
 public class InitiateSearch {
 
-    public static void handleSearchBar(final Context context, final CardView search, final Toolbar toolbarMain, final View view, final EditText editText, final ImageView back, final ImageView clear, final DrawerLayout mDrawerLayout) {
+    public static void handleSearchBar(final Context context, final CardView search, final Toolbar toolbarMain, final View view, final EditText editText, final ImageView back, final ImageView clear, final AppBarLayout appBar, final TabLayout tablayout) {
         final Animation fade_in = AnimationUtils.loadAnimation(context.getApplicationContext(), android.R.anim.fade_in);
         final Animation fade_out = AnimationUtils.loadAnimation(context.getApplicationContext(), android.R.anim.fade_out);
-        final DrawerArrowDrawable toolbarDrawable = new DrawerArrowDrawable(context);
-        final ValueAnimator toolbarAnimator = ValueAnimator.ofFloat(0f, 1f);
-        toolbarAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override public void onAnimationUpdate(ValueAnimator animation) {
-                toolbarDrawable.setProgress((Float)animation.getAnimatedValue());
-            }
-        });
-
-        final DrawerArrowDrawable searchbarDrawable = new DrawerArrowDrawable(context);
-        final ValueAnimator searchbarAnimator = ValueAnimator.ofFloat(0f, 1f);
-        searchbarAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override public void onAnimationUpdate(ValueAnimator animation) {
-                searchbarDrawable.setProgress((Float)animation.getAnimatedValue());
-            }
-        });
-
-        toolbarMain.setNavigationIcon(toolbarDrawable);
-        back.setImageDrawable(searchbarDrawable);
 
         if (search.getVisibility() == View.VISIBLE) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -55,12 +41,10 @@ public class InitiateSearch {
                 animatorHide.addListener(new Animator.AnimatorListener() {
                     @Override
                     public void onAnimationStart(Animator animation) {
-                        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
                         editText.startAnimation(fade_out);
+                        appBar.startAnimation(fade_out);
+                        tablayout.startAnimation(fade_out);
                         clear.startAnimation(fade_out);
-                        toolbarMain.setNavigationIcon(toolbarDrawable);
-                        toolbarAnimator.reverse();
-                        searchbarAnimator.reverse();
                     }
 
                     @Override
@@ -86,9 +70,6 @@ public class InitiateSearch {
                 ((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(view.getWindowToken(), 0);
                 view.setVisibility(View.INVISIBLE);
                 search.setVisibility(View.GONE);
-                toolbarMain.setNavigationIcon(toolbarDrawable);
-                toolbarAnimator.reverse();
-                searchbarAnimator.reverse();
                 editText.startAnimation(fade_out);
                 clear.startAnimation(fade_out);
                 search.startAnimation(fade_out);
@@ -107,21 +88,24 @@ public class InitiateSearch {
                 animator.addListener(new Animator.AnimatorListener() {
                     @Override
                     public void onAnimationStart(Animator animation) {
-                        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
                         toolbarMain.getMenu().clear();
                         view.setVisibility(View.VISIBLE);
                         editText.setVisibility(View.INVISIBLE);
                         clear.setVisibility(View.INVISIBLE);
-                        toolbarAnimator.start();
-                        searchbarAnimator.start();
+                        tablayout.setVisibility(View.INVISIBLE);
+                        appBar.setVisibility(View.INVISIBLE);
                     }
 
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         editText.setVisibility(View.VISIBLE);
                         clear.setVisibility(View.VISIBLE);
+                        tablayout.setVisibility(View.VISIBLE);
+                        appBar.setVisibility(View.VISIBLE);
                         editText.startAnimation(fade_in);
-                        clear.startAnimation(fade_in);
+                        editText.startAnimation(fade_in);
+                        tablayout.startAnimation(fade_in);
+                        appBar.startAnimation(fade_in);
                         ((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
                     }
 
@@ -142,8 +126,6 @@ public class InitiateSearch {
                     search.setEnabled(true);
                 }
             } else {
-                toolbarAnimator.start();
-                searchbarAnimator.start();
                 search.setVisibility(View.VISIBLE);
                 search.setEnabled(true);
                 ((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
--- app/src/main/java/com/xelitexirish/scammerbingo/ui/MainActivity.java
+++ app/src/main/java/com/xelitexirish/scammerbingo/ui/MainActivity.java
@@ -1,23 +1,48 @@
 package com.xelitexirish.scammerbingo.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
+import com.xelitexirish.scammerbingo.InitiateSearch;
 import com.xelitexirish.scammerbingo.R;
 import com.xelitexirish.scammerbingo.prefs.PreferenceHandler;
 import com.xelitexirish.scammerbingo.util.DataHelper;
@@ -50,11 +75,29 @@ public class MainActivity extends AppCompatActivity {
             button20;
 
     private Toolbar mToolbar;
+    public ViewPager viewPager;
+    public TabLayout tabLayout;
+    private InitiateSearch initiateSearch;
+    private CardView card_search;
+    private ImageView image_search_back, clearSearch;
+    private EditText edit_text_search;
+    private RelativeLayout view_search;
+    private DrawerLayout mDrawerLayout;
+    private AppBarLayout mSearchToolbar;
 
     AdView footerAdview;
 
     public static int score = 0;
     public static Button[] allButtons;
+
+    public final String NUMBERS_TAG = "numbers";
+    public final String WEBSITES_TAG = "websites";
+    public final String IP_TAG = "ipAddresses";
+
+    public final String NUMBERS_TITLE = "Numbers";
+    public final String WEBSITES_TITLE = "Websites";
+    public final String IP_TITLE = "IPs";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout_main);
+        mSearchToolbar = (AppBarLayout) findViewById(R.id.search_toolbar);
 
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
@@ -113,6 +157,70 @@ public class MainActivity extends AppCompatActivity {
         }
 
         DataHelper.inflateLists();
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.search_tabs);
+
+        tabLayout.addTab(tabLayout.newTab().setTag(NUMBERS_TAG).setText(NUMBERS_TITLE));
+        tabLayout.addTab(tabLayout.newTab().setTag(WEBSITES_TAG).setText(WEBSITES_TITLE));
+        tabLayout.addTab(tabLayout.newTab().setTag(IP_TAG).setText(IP_TITLE));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        final PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        edit_text_search = (EditText) findViewById(R.id.edit_text_search);
+        card_search = (CardView) findViewById(R.id.card_search);
+        image_search_back = (ImageView) findViewById(R.id.image_search_back);
+        clearSearch = (ImageView) findViewById(R.id.clearSearch);
+        view_search = (RelativeLayout) findViewById(R.id.view_search);
+
+        clearSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edit_text_search.getText().toString().length() == 0) {
+                } else {
+                    edit_text_search.setText("");
+                    ((InputMethodManager) MainActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+                }
+            }
+        });
+
+        image_search_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initiateSearch.handleSearchBar(MainActivity.this, card_search, mToolbar, view_search, edit_text_search, image_search_back, clearSearch, mSearchToolbar, tabLayout);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (card_search.getVisibility() == View.VISIBLE) {
+            initiateSearch.handleSearchBar(MainActivity.this, card_search, mToolbar, view_search, edit_text_search, image_search_back, clearSearch, mSearchToolbar, tabLayout);
+            ((InputMethodManager) MainActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -130,7 +238,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_header, menu);
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -139,8 +246,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_scammer_details:
-                Intent searchIntent = new Intent(this, DialogScammerList.class);
-                startActivity(searchIntent);
+                initiateSearch.handleSearchBar(MainActivity.this, card_search, mToolbar, view_search, edit_text_search, image_search_back, clearSearch, mSearchToolbar, tabLayout);
+                new MaterialDialog.Builder(this)
+                        .title("Warning")
+                        .content("The information you see here is scammer information such as phone numbers, websites, and IP addresses. Please be careful.")
+                        .positiveText("I Understand")
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                //Dismiss Dialog
+                            }
+                        })
+                        .negativeText("Go Back")
+                        .onNegative(new MaterialDialog.SingleButtonCallback() {
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                //Go Back
+                                finish();
+                            }
+                        }).show();
                 break;
 
             case R.id.action_share:
@@ -254,5 +376,173 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return currentButtonTexts;
+    }
+
+
+    public class PageAdapter extends FragmentPagerAdapter {
+        private int numTabs;
+
+        public PageAdapter(FragmentManager fragmentManager, int tabs) {
+            super(fragmentManager);
+            numTabs = tabs;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return NumbersTab.newInstance();
+                case 1:
+                    return WebsitesTab.newInstance();
+                case 2:
+                    return IpTab.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return numTabs;
+        }
+    }
+
+    public static class NumbersTab extends Fragment {
+
+        ListView listViewScammers;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.dialog_scammer_list_view, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            this.listViewScammers = (ListView) view.findViewById(R.id.listViewScammers);
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getContext(), R.layout.item_list_bullet, DataHelper.numbersList);
+            this.listViewScammers.setAdapter(arrayAdapter);
+
+            this.listViewScammers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final String phoneNumber = DataHelper.numbersList.get(position);
+
+                    final MaterialDialog.Builder alertDialogConfirm = new MaterialDialog.Builder(getContext());
+                    alertDialogConfirm.title("Are you sure you want to call this phone number?");
+                    alertDialogConfirm.content("This number is known for contacting scammers, are you sure you want to call?");
+                    alertDialogConfirm.positiveText("Call");
+                    alertDialogConfirm.onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            try {
+                                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                                startActivity(intent);
+                            } catch (Exception e) {
+                                Toast.makeText(getContext(), "Unable to call phone number", Toast.LENGTH_SHORT).show();
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    alertDialogConfirm.negativeText("Dismiss");
+                    alertDialogConfirm.onNegative(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            // dismiss
+                        }
+                    });
+                    alertDialogConfirm.show();
+                }
+            });
+        }
+
+        public static NumbersTab newInstance() {
+            NumbersTab numbersTab = new NumbersTab();
+            return numbersTab;
+        }
+    }
+
+    public static class WebsitesTab extends Fragment {
+
+        ListView listViewScammers;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.dialog_scammer_list_view, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            this.listViewScammers = (ListView) view.findViewById(R.id.listViewScammers);
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getContext(), R.layout.item_list_bullet, DataHelper.websitesList);
+            this.listViewScammers.setAdapter(arrayAdapter);
+
+            this.listViewScammers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final String url = DataHelper.websitesList.get(position);
+
+                    final MaterialDialog.Builder alertDialogConfirm = new MaterialDialog.Builder(getContext());
+                    alertDialogConfirm.title("Are you sure you want to open this URL?");
+                    alertDialogConfirm.content("This website is known for advertising scammers, are you sure you want to open it?");
+                    alertDialogConfirm.positiveText("Open");
+                    alertDialogConfirm.onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + url));
+                                getContext().startActivity(intent);
+                            } else {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                                getContext().startActivity(intent);
+                            }
+                        }
+                    });
+                    alertDialogConfirm.negativeText("Dismiss");
+                    alertDialogConfirm.onNegative(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            // dismiss
+                        }
+                    });
+                    alertDialogConfirm.show();
+                }
+            });
+        }
+
+        public static WebsitesTab newInstance() {
+            WebsitesTab websitesTab = new WebsitesTab();
+            return websitesTab;
+        }
+    }
+
+    public static class IpTab extends Fragment {
+
+        ListView listViewScammers;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.dialog_scammer_list_view, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            listViewScammers = (ListView) view.findViewById(R.id.listViewScammers);
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getContext(), R.layout.item_list_bullet, DataHelper.ipsList);
+            listViewScammers.setAdapter(arrayAdapter);
+        }
+
+        public static IpTab newInstance() {
+            IpTab ipTab = new IpTab();
+            return ipTab;
+        }
     }
 }
