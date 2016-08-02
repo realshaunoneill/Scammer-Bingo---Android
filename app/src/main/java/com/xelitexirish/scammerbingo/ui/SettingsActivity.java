accc0fac8c374783b26ee8875a62d3bc96d3e420
XeliteXirish <shaunoneill7@me.com>
2016-08-02 20:13:42 +0100

    Reverted to older version of settings activity to fix a bug

--- app/src/main/java/com/xelitexirish/scammerbingo/ui/SettingsActivity.java
+++ app/src/main/java/com/xelitexirish/scammerbingo/ui/SettingsActivity.java
@@ -2,16 +2,12 @@ package com.xelitexirish.scammerbingo.ui;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.xelitexirish.scammerbingo.R;
 
@@ -37,41 +33,33 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
     private void setupSimplePreferencesScreen() {
         if (isSimplePreferences(this)) {
+            addPreferencesFromResource(R.xml.pref_general);
 
+            setupPreferenceClickListeners();
         }
     }
 
-    public static class GeneralFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+    private void setupPreferenceClickListeners() {
 
-            addPreferencesFromResource(R.xml.pref_general);
-            setupPreferenceClickListeners();
-        }
+        findPreference("CACHE_ONLINE_LIST").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Snackbar.make(findViewById(android.R.id.content), "Coming soon, I promise", Snackbar.LENGTH_SHORT).show();
+                return false;
+            }
+        });
 
-        private void setupPreferenceClickListeners() {
-
-            findPreference("CACHE_ONLINE_LIST").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    Snackbar.make(getActivity().findViewById(android.R.id.content), "Coming soon, I promise", Snackbar.LENGTH_SHORT).show();
-                    return false;
+        findPreference("ENABLE_SOUNDS").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                if ((boolean) newValue) {
+                    Snackbar.make(findViewById(android.R.id.content), "Sounds enabled", Snackbar.LENGTH_SHORT).show();
+                } else {
+                    Snackbar.make(findViewById(android.R.id.content), "Sounds disabled", Snackbar.LENGTH_SHORT).show();
                 }
-            });
-
-            findPreference("ENABLE_SOUNDS").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    if ((boolean) newValue) {
-                        Snackbar.make(getActivity().findViewById(android.R.id.content), "Sounds enabled", Snackbar.LENGTH_SHORT).show();
-                    } else {
-                        Snackbar.make(getActivity().findViewById(android.R.id.content), "Sounds disabled", Snackbar.LENGTH_SHORT).show();
-                    }
-                    return true;
-                }
-            });
-        }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -85,20 +73,5 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
     private static boolean isSimplePreferences(Context context) {
         return Build.VERSION.SDK_INT < 11 || !isXLargeTablet(context);
-    }
-
-    @Override
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.pref_headers, target);
-    }
-
-    /**
-     * This method stops fragment injection in malicious applications.
-     * Make sure to deny any unknown fragments here.
-     */
-    protected boolean isValidFragment(String fragmentName) {
-        return PreferenceFragment.class.getName().equals(fragmentName)
-                || GeneralFragment.class.getName().equals(fragmentName);
     }
 }
