3e965b93cb427bc954587e908245b197542dcdf3
Andrew Fluck <Andrew Fluck>
2016-08-03 11:49:10 -0400

    Navigation Drawer Base

--- app/src/main/java/com/xelitexirish/scammerbingo/ui/MainActivity.java
+++ app/src/main/java/com/xelitexirish/scammerbingo/ui/MainActivity.java
@@ -2,8 +2,12 @@ package com.xelitexirish.scammerbingo.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -11,6 +15,8 @@ import com.xelitexirish.scammerbingo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
     private Toolbar mToolbar;
 
     public static int score = 0;
@@ -20,31 +26,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mToolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout_main);
+        mNavigationView = (NavigationView) findViewById(R.id.navigationview_main);
     }
 
-    public void onButtonPressed(Button pressedButton){
-        if(score != allButtons.length){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onButtonPressed(Button pressedButton) {
+        if (score != allButtons.length) {
             score++;
-        }else{
+        } else {
 
         }
         pressedButton.setEnabled(false);
         updateScore();
     }
 
-    public void updateScore(){
+    public void updateScore() {
         getSupportActionBar().setSubtitle(getString(R.string.score) + ": " + score + "/" + allButtons.length);
 
-        if(score == allButtons.length){
+        if (score == allButtons.length) {
             MaterialDialog.Builder alertDialogComplete = new MaterialDialog.Builder(this);
             alertDialogComplete.title(getString(R.string.bingo_complete_title));
             alertDialogComplete.content(getString(R.string.bingo_complete_msg));
             alertDialogComplete.positiveText(getString(R.string.action_okay));
             alertDialogComplete.show();
-        }else if(score == allButtons.length / 2){
+        } else if (score == allButtons.length / 2) {
             MaterialDialog.Builder alertDialogHalf = new MaterialDialog.Builder(this);
             alertDialogHalf.title(getString(R.string.bingo_half_title));
             alertDialogHalf.content(getString(R.string.bingo_half_msg));
@@ -53,13 +71,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void resetScore(){
+    public void resetScore() {
         score = 0;
         updateScore();
         setButtonsEnabled();
     }
 
-    public void setButtonsEnabled(){
+    public void setButtonsEnabled() {
         for (int x = 0; x < allButtons.length; x++) {
             Button button = allButtons[x];
             button.setEnabled(true);
