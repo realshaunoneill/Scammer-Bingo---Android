0f4f70a6476bf356d87b555fe293d1635b198491
XeliteXirish <shaunoneill7@me.com>
2016-08-03 18:36:49 +0100

    Preference handler

--- app/src/main/java/com/xelitexirish/scammerbingo/prefs/PreferenceHandler.java
+++ app/src/main/java/com/xelitexirish/scammerbingo/prefs/PreferenceHandler.java
@@ -1,7 +1,18 @@
 package com.xelitexirish.scammerbingo.prefs;
 
-/**
- * Created by Shaun on 03/08/2016.
- */
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 public class PreferenceHandler {
+
+    public static boolean areSoundsEnabled(Context context){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean("ENABLE_SOUNDS", true);
+    }
+
+    public static boolean enableAutoReset(Context context){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean("ENABLE_AUTO_RESET", false);
+    }
 }
