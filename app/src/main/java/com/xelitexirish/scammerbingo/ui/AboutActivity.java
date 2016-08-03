b7ea24279669b34610b67091cec71077352470ae
Andrew Fluck <andrewtfluck@gmail.com>
2016-08-03 14:32:44 -0400

    Added all the intents in about

--- app/src/main/java/com/xelitexirish/scammerbingo/ui/AboutActivity.java
+++ app/src/main/java/com/xelitexirish/scammerbingo/ui/AboutActivity.java
@@ -50,7 +50,7 @@ public class AboutActivity extends AppCompatActivity{
         mVersion = (TextView) findViewById(R.id.version_text);
         mVersion.setText("Version: " + appVersion);
 
-        // XeliteXirish
+        //XeliteXirish
         mAuthorGithub = (Button) findViewById(R.id.author_git);
         mAuthorGithub.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +72,126 @@ public class AboutActivity extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 Snackbar.make(mCoordinatorLayout, "Coming soon, I promise", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        //Newtsrock
+        mDesignerGithub = (Button) findViewById(R.id.designer_git);
+        mDesignerGithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.github.com/Newtsrock"));
+                startActivity(intent);
+            }
+        });
+        mDesignerTwitter = (Button) findViewById(R.id.designer_twitter);
+        mDesignerTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/newtsrock"));
+                startActivity(intent);
+            }
+        });
+        mDesignerWebsite = (Button) findViewById(R.id.designer_site);
+        mDesignerWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://newtsrock.github.io"));
+                startActivity(intent);
+            }
+        });
+
+        //JoeTheHuman
+        mPromo1Github = (Button) findViewById(R.id.promo_1_git);
+        mPromo1Github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/JoeTheHuman"));
+                startActivity(intent);
+            }
+        });
+
+        mPromo1YouTube = (Button) findViewById(R.id.promo_1_youtube);
+        mPromo1YouTube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCF2tv9jPDtgmCSvg05-C21A"));
+                startActivity(intent);
+            }
+        });
+
+        mPromo1Twitter = (Button) findViewById(R.id.promo_1_twitter);
+        mPromo1Twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/joethehumandev"));
+                startActivity(intent);
+            }
+        });
+
+        //Hexxium
+        mPromo2Github = (Button) findViewById(R.id.promo_2_git);
+        mPromo2Github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Hexxium"));
+                startActivity(intent);
+            }
+        });
+
+        mPromo2YouTube = (Button) findViewById(R.id.promo_2_youtube);
+        mPromo2YouTube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCeBAWpe_uezAQTzz_fCO6EQ"));
+                startActivity(intent);
+            }
+        });
+
+        mPromo2Twitter = (Button) findViewById(R.id.promo_2_twitter);
+        mPromo2Twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/HexxiumGaming"));
+                startActivity(intent);
+            }
+        });
+
+        //Dazza
+        mPromo3YouTube = (Button) findViewById(R.id.promo_3_youtube);
+        mPromo3YouTube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCzVm0R21dxt8wM2mrZNwOZw"));
+                startActivity(intent);
+            }
+        });
+
+        mPromo3Twitter = (Button) findViewById(R.id.promo_3_twitter);
+        mPromo3Twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/OhDxzza"));
+                startActivity(intent);
+            }
+        });
+
+        //Lewis
+        mPromo4YouTube = (Button) findViewById(R.id.promo_4_youtube);
+        mPromo4YouTube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/user/LewissTech"));
+                startActivity(intent);
+            }
+        });
+
+        mPromo4Twitter = (Button) findViewById(R.id.promo_4_twitter);
+        mPromo4Twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/lewisstechYT"));
+                startActivity(intent);
             }
         });
     }
