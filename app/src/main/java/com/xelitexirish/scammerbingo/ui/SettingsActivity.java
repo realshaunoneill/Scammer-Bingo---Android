01c87c0f8ab8a8ebd01795bed3900edbe09b0d6e
Andrew Fluck <andrewtfluck@gmail.com>
2016-08-03 13:30:46 -0400

    theme fix for settings

--- app/src/main/java/com/xelitexirish/scammerbingo/ui/SettingsActivity.java
+++ app/src/main/java/com/xelitexirish/scammerbingo/ui/SettingsActivity.java
@@ -135,6 +135,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      * {@inheritDoc}
      */
